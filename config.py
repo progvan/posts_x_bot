5cf06c29e2baca5e287468de5622876c861f6fff
progvan <vanatom22@gmail.com>
2024-12-08 23:04:37 +0100

    add Twitter integration and an example of reading the last 5 tweets of a user

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 TOKEN = "7510353092:AAFsxM6WiFAlQ37lCNc8_DW8SBzmuqHmnbw"
-
+x_bearer_token = "AAAAAAAAAAAAAAAAAAAAAFYLxQEAAAAAEk%2BAgChsemupdj6kH%2FQ8wW5u6HA%3DP5s2HkkhSaaCrSXGNR4UbOwtbAowitqEduFh9oG6IC2Vmr6O2J"
+x_key = "toXXzeEtr9fS2pH59dVlLarIs" 
+x_key_secret = "ZqcOGMTpOh0XUf5TplKc0NqCrBpr2jjuRxjb2efEbDfIrdc2K0"
+x_token = "1856297588233789440-hGU5oTP3tEv90QJphN48p1nrew1EET" 
+x_token_secret = "ORYiVcpuoJcgXadKTalIfPF8YImnNeiIM1Ihira0VrDTG"
 
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-import telebot
-from telebot import types
-
 import config
 import answer
 import core
+import telebot
+import tweepy
+from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
+client = tweepy.Client(bearer_token=config.x_bearer_token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-   bot.send_message(message.chat.id, answer.TEXT_WELCOME_USERS, reply_markup=types.ReplyKeyboardRemove())
+    bot.send_message(message.chat.id, get_latest_tweets())
+    bot.send_message(message.chat.id, answer.TEXT_WELCOME_USERS, reply_markup=types.ReplyKeyboardRemove())
 
-bot.polling(none_stop = True)
+def get_latest_tweets():
+    try:
+        response = client.get_users_tweets(id='44196397', max_results=5) # Elon Musks user id (for testing)
+        tweets = response.data
+        tweets_text = "\n\n".join([f"@elonmusk said: {tweet.text}" for tweet in tweets])
+        return tweets_text
+    except Exception as e:
+        return f"Error: {e}"
+
+def get_user_id(username):
+    try:
+        user = client.get_user(username=username)
+        return user.data.id
+    except Exception as e:
+        return f"Error: {e}"
+
+bot.polling(none_stop=True)
